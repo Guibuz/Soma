10be38fb9179bd3e298bed291e4a4fe2c0f0276b
Guibuz <86129989+Guibuz@users.noreply.github.com>
2021-06-18 20:38:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,41 +2,41 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-void soma()
+void soma() //função de soma
 {
     int a,b,c;
     printf("Digite o numero que deseja somar\n");
     scanf("%d", &a);
     printf("Digite o segundo numero que deseja somar\n");
     scanf("%d", &b);
-    while (a != 0) 
+    while (a != 0) //Laço de repetição para rodar até chegar no resultado
     {
-        c = b & a;
-        b = b ^ a;
-        c = c << 1;
+        c = b & a; //c recebe o valor de and de b & a
+        b = b ^ a; //b recebe o valor de xor de b & a
+        c = c << 1; //c recebe a mudança de bit de c
         a = c;
     }
     printf("Soma: %d\n",b);
 }
 
-void sub()
+void sub() //funcao de subtração
 {
     int a,b,c;
     printf("Digite o numero que deseja subtrair\n");
     scanf("%d", &a);
     printf("Digite o segundo numero que deseja subtrair\n");
     scanf("%d", &b);
-    while (b != 0) 
+    while (b != 0) //Laço de repetição para rodar até chegar no resultado
     {
-        a = a ^ b;
-        c = a & b;
-        b = c << 1;
+        a = a ^ b; //a recebe o valor de xor de a & b
+        c = a & b; //c recebe o valor de and de a & b
+        b = c << 1; //b recebe a mudança de bit do c
         c = a;
     }
     printf("sub: %d\n",c);
 }
 
-int main(){
+int main(){ //função principal
   soma();
   sub();
   return 0;
